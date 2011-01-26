7f1f969a2ae794735089a83c2578e0c44dbcd67f
Kilo Force <kiloforce@k.ttak.org>
2011-01-26 12:54:55 -0700

    rename and move files

--- tztk-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo -n "Previous hash: "
-md5sum minecraft_server.jar
-wget -N -nv http://minecraft.net/download/minecraft_server.jar
-echo -n "New hash:      "
-md5sum minecraft_server.jar
